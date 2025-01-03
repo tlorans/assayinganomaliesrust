77ed73b1f77a5cfce49fe64e52d4310ec5c56ca6
Your Name <you@example.com>
2025-01-03 16:34:57 +0100

    get crsp_data

--- src/utilities/data_download.rs
+++ src/utilities/data_download.rs
@@ -7,115 +7,9 @@ use polars::prelude::*;
 use postgres_native_tls::MakeTlsConnector;
 use std::collections::HashMap;
 use std::env;
+use std::fs;
 use std::fs::File;
 use tokio_postgres::Client;
-use std::fs;
-
-pub async fn download_crsp_monthly(
-    config: &WrdsConfig,
-    start_date: &str,
-    end_date: &str,
-) -> Result<DataFrame> {
-    let client = establish_connection(config).await?;
-    let query = format!(
-        "SELECT msf.permno, date_trunc('month', msf.mthcaldt)::date AS date, \
-    msf.mthret::double precision AS ret, \
-    msf.shrout::double precision AS shrout, \
-    msf.mthprc::double precision AS altprc, \
-    ssih.primaryexch, ssih.siccd \
-    FROM crsp.msf_v2 AS msf \
-    INNER JOIN crsp.stksecurityinfohist AS ssih \
-    ON msf.permno = ssih.permno AND \
-       ssih.secinfostartdt <= msf.mthcaldt AND \
-       msf.mthcaldt <= ssih.secinfoenddt \
-    WHERE msf.mthcaldt BETWEEN '{}' AND '{}' \
-          AND ssih.sharetype = 'NS' \
-          AND ssih.securitytype = 'EQTY' \
-          AND ssih.securitysubtype = 'COM' \
-          AND ssih.usincflg = 'Y' \
-          AND ssih.issuertype in ('ACOR', 'CORP') \
-          AND ssih.primaryexch in ('N', 'A', 'Q') \
-          AND ssih.conditionaltype in ('RW', 'NW') \
-          AND ssih.tradingstatusflg = 'A'",
-        start_date, end_date
-    );
-    info!("Downloading CRSP monthly data");
-    let rows = client.query(query.as_str(), &[]).await?;
-    info!("Fetched {} rows from CRSP monthly data", rows.len());
-    // Ok(rows)
-
-    let mut permno: Vec<i32> = Vec::with_capacity(rows.len());
-    let mut date: Vec<chrono::NaiveDate> = Vec::with_capacity(rows.len());
-    let mut ret: Vec<Option<f64>> = Vec::with_capacity(rows.len());
-    let mut shrout: Vec<Option<f64>> = Vec::with_capacity(rows.len());
-    let mut altprc: Vec<Option<f64>> = Vec::with_capacity(rows.len());
-    let mut primaryexch: Vec<Option<String>> = Vec::with_capacity(rows.len());
-    let mut siccd: Vec<Option<i32>> = Vec::with_capacity(rows.len());
-
-    for row in rows {
-        permno.push(row.get("permno"));
-        date.push(row.get("date"));
-        ret.push(row.get("ret"));
-        shrout.push(row.get("shrout"));
-        altprc.push(row.get("altprc"));
-        primaryexch.push(row.get("primaryexch"));
-        siccd.push(row.get("siccd"));
-    }
-
-    let mut df: DataFrame = df![
-        "permno" => permno,
-        "date" => date,
-        "ret" => ret,
-        "shrout" => shrout,
-        "altprc" => altprc,
-        "primaryexch" => primaryexch,
-        "siccd" => siccd,
-    ]?;
-
-    df = df
-        .lazy()
-        .with_column(
-            when((col("shrout") * col("altprc") / lit(1_000_000)).eq(lit(0.0)))
-                .then(lit(f64::NAN))
-                .otherwise(col("shrout") * col("altprc") / lit(1_000_000))
-                .alias("mktcap"),
-        )
-        .collect()?;
-
-    // Additional transformations (exchange mapping, industry mapping)
-    df = map_industry(df)?;
-
-    Ok(df)
-}
-
-fn map_industry(mut df: DataFrame) -> Result<DataFrame> {
-    let industry_map = |siccd: i32| -> &str {
-        match siccd {
-            1..=999 => "Agriculture",
-            1000..=1499 => "Mining",
-            1500..=1799 => "Construction",
-            2000..=3999 => "Manufacturing",
-            4000..=4899 => "Transportation",
-            4900..=4999 => "Utilities",
-            5000..=5199 => "Wholesale",
-            5200..=5999 => "Retail",
-            6000..=6799 => "Finance",
-            7000..=8999 => "Services",
-            9000..=9999 => "Public",
-            _ => "Missing",
-        }
-    };
-
-    let industry: Vec<&str> = df
-        .column("siccd")?
-        .i32()?
-        .into_iter()
-        .map(|opt| opt.map(industry_map).unwrap_or("Missing"))
-        .collect();
-
-    df.with_column(Series::new("industry".into(), &industry))?;
-    Ok(df)
-}
 
 #[derive(Debug)]
 pub struct WrdsConfig {
@@ -215,12 +109,13 @@ pub async fn get_wrds_table(
         let current_series = match data_type.name() {
             // if date, convert to Vec<chrono>
             "date" => {
-                let col_data: Vec<Option<chrono::NaiveDate>> = rows.iter().map(|row| row.get(idx)).collect();
+                let col_data: Vec<Option<chrono::NaiveDate>> =
+                    rows.iter().map(|row| row.get(idx)).collect();
                 Column::new(col_name.clone(), Series::new(col_name, col_data))
             }
             "int2" => {
-            let col_data: Vec<Option<i16>> = rows.iter().map(|row| row.get(idx)).collect();
-            Column::new(col_name.clone(), Series::new(col_name, col_data))
+                let col_data: Vec<Option<i16>> = rows.iter().map(|row| row.get(idx)).collect();
+                Column::new(col_name.clone(), Series::new(col_name, col_data))
             }
             "int4" => {
                 let col_data: Vec<Option<i32>> = rows.iter().map(|row| row.get(idx)).collect();
@@ -276,29 +171,29 @@ pub async fn get_wrds_table(
     Ok(())
 }
 
+pub async fn get_crsp_data(client: &Client, dir_path: &str, output_format: &str) -> Result<()> {
+    // Download required tables
+    let tables = [
+        ("CRSP", "MSFHDR"),    //
+        ("CRSP", "MSF"),       // Main dataset
+        ("CRSP", "MSEDELIST"), // delisting returns
+        ("CRSP", "MSEEXCHDATES"),
+        ("CRSP", "CCMXPF_LNKHIST"),
+        ("CRSP", "STOCKNAMES"),
+    ];
+
+    // Specify output directory and format
+    for (libname, memname) in &tables {
+        get_wrds_table(&client, libname, memname, dir_path, None, output_format)
+            .await
+            .unwrap();
+    }
+    Ok(())
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[tokio::test]
-    async fn test_download_crsp_monthly() {
-        let config = WrdsConfig::from_env();
-        let start_date = "2020-01-01";
-        let end_date = "2020-01-31";
-        let mut df: DataFrame = download_crsp_monthly(&config, start_date, end_date)
-            .await
-            .unwrap();
-
-        let mut file = std::fs::File::create("test.parquet").unwrap();
-        ParquetWriter::new(&mut file).finish(&mut df).unwrap();
-
-        // Read the parquet file
-        let mut readFile = std::fs::File::open("test.parquet").unwrap();
-        let read_df = ParquetReader::new(&mut readFile).finish().unwrap();
-
-        dbg!(&read_df);
-    }
 
     #[tokio::test]
     async fn test_get_crsp() {
@@ -306,12 +201,12 @@ mod test {
 
         // Download required tables
         let tables = [
-            ("CRSP", "MSFHDR"), // 
-            // ("CRSP", "MSF"), // Main dataset
-            // ("CRSP", "MSEDELIST"), // delisting returns
-            // ("CRSP", "MSEEXCHDATES"),
-            // ("CRSP", "CCMXPF_LNKHIST"),
-            // ("CRSP", "STOCKNAMES"),
+            ("CRSP", "MSFHDR"), //
+                                // ("CRSP", "MSF"), // Main dataset
+                                // ("CRSP", "MSEDELIST"), // delisting returns
+                                // ("CRSP", "MSEEXCHDATES"),
+                                // ("CRSP", "CCMXPF_LNKHIST"),
+                                // ("CRSP", "STOCKNAMES"),
         ];
 
         let client = establish_connection(&config).await.unwrap();
